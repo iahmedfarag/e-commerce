79a172319a217165d064f226e0dd9561f387fb98
iahmedfarag <ahmedfarag1k@gmail.com>
2023-04-13 06:10:50 +0200

    payment callback url

--- src/context.jsx
+++ src/context.jsx
@@ -253,7 +253,7 @@ export const AppProvider = ({ children }) => {
     setIsLoading(true);
     try {
       let { data } = await axios.post(
-        `https://route-ecommerce-app.vercel.app/api/v1/orders/checkout-session/${id}?url=https://creator-ecommerce.netlify.app/`,
+        `https://route-ecommerce-app.vercel.app/api/v1/orders/checkout-session/${id}?url=https://creator-ecommerce.netlify.app`,
         { ShippingAddress },
         { headers }
       );
